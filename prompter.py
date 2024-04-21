9bb0465b495a8735afa13ff80c25e1fdf8428cf1
Sudharshana V P <sudharshanavp@outlook.com>
2024-04-21 12:18:28 +0530

    prompt finetuning

--- prompter.py
+++ prompter.py
@@ -21,7 +21,7 @@ def generate(no_of_cases, regex):
     vertexai.init(project="refined-outlet-420415", location="us-central1")
     model = GenerativeModel("gemini-1.0-pro-vision-001")
 
-    input_prompt = f'You are a software QA expert, I want you to write a set of {no_of_cases} test cases to test if the regex is correctly witten for an entity with given requirement. The given requirements is must be a "{regex}" Please cover the edge cases and corner cases as much as possible while generating the test cases and segregate them in positive and negative classes. Generate the output in a format easy to display on a webpage such as a markdown table.'
+    input_prompt = f'You are a software QA expert, I want you to write a set of {no_of_cases} test cases to test if the regex is correctly witten for an entity with given requirement. The given requirements is must be a "{regex}" Please cover the edge cases and corner cases as much as possible while generating the test cases and segregate them in positive and negative classes. Generate the output in a format easy to display on a webpage such as a markdown table. Do not include Regex in the output. The output has to always be a markdown table with the columns being "Test Case" "Input" "Expected Result"'
 
     responses = model.generate_content(
         [input_prompt],
