d63322b3ea869cbce2b1ce383ca4c7a8a02b773e
Sudharshana V P <sudharshanavp@outlook.com>
2024-04-21 12:18:54 +0530

    added converting markdown to dataframe feature

--- utils/common_utils.py
+++ utils/common_utils.py
@@ -1,6 +1,17 @@
-def formatter(response):
+import re
+import pandas as pd
+
+def response_to_markdown(response):
     markdown_string = ""
     for data in response:
         markdown_string += data.text
     
     return markdown_string
+
+def markdown_to_df(markdown):
+    pattern = r"\| ([\w\s]+) \| ([\w\s]+) \| ([\w\s]+) \|"
+    matched_data = re.findall(pattern, markdown)
+    header = matched_data[0]
+    data = matched_data[1:]
+    df = pd.DataFrame(data, columns=header)
+    return df
